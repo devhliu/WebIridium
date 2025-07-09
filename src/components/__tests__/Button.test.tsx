890770c39d9a661e35eb21080b44444a40c596f9
qndan <80284796+qndan@users.noreply.github.com>
2025-07-08 18:59:04 -0700

    add button test

---
+++ src/components/__tests__/Button.test.tsx
@@ -0,0 +1,29 @@
+import { it, expect, vi } from "vitest";
+import { screen, render } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+import Button from "../Button";
+
+it("should call onClick", async () => {
+  const onClick = vi.fn();
+  render(
+    <Button onClick={onClick}>test</Button>
+  );
+
+  const button = screen.getByText("test");
+  await userEvent.click(button);
+
+  expect(onClick).toHaveBeenCalledOnce();
+});
+
+it("should be able to be disabled", async () => {
+  const onClick = vi.fn();
+  render(
+    <Button onClick={onClick} disabled>test</Button>
+  );
+
+  const button = screen.getByText("test");
+  await userEvent.click(button);
+
+  expect(button).toBeDisabled();
+  expect(onClick).not.toHaveBeenCalled();
+});
