23ba0d91290cf131f050db4026dfef5173473278
qndan <80284796+qndan@users.noreply.github.com>
2025-08-27 03:11:05 -0700

    Revert "??? weird test"
    This reverts commit 50a0df6dddbb4884739e1e953332d0041c75e0e7.

--- src/app/panels/__tests__/testButton.tsx
+++ src/app/panels/__tests__/testButton.tsx
@@ -87,11 +87,9 @@ export const itShouldBeCancellable = ({
     const cancel = screen.getByLabelText("Cancel");
     await userEvent.click(cancel);
 
-    await waitFor(() => {
-      expect(button).toBeEnabled();
-      expect(cancel).not.toBeInTheDocument();
-      expect(screen.queryByTestId("results-plot")).not.toBeInTheDocument();
-    });
+    expect(button).toBeEnabled();
+    expect(cancel).not.toBeInTheDocument();
+    expect(screen.queryByTestId("results-plot")).not.toBeInTheDocument();
   });
 };
 
