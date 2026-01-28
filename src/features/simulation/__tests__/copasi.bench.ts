a870383598e3ea3e1c8183d87df67ee21b0d77b3
qndan <80284796+qndan@users.noreply.github.com>
2026-01-28 14:42:52 -0800

    simpleify benchmark

--- src/features/simulation/__tests__/copasi.bench.ts
+++ src/features/simulation/__tests__/copasi.bench.ts
@@ -1,4 +1,4 @@
-import { bench, vi } from "vitest";
+import { describe, bench, vi } from "vitest";
 vi.unmock("@/features/workers");
 import { CopasiSimulator } from "../CopasiSimulator";
 
@@ -7,102 +7,36 @@ import kholodenkoMedium from "./kholodenko_medium.ant?raw";
 import firczukLarge from "./firczuk_large.ant?raw";
 import smallboneXlarge from "./smallbone_xlarge.ant?raw";
 
-{
-  const simulator = new CopasiSimulator();
-  const variables = await simulator.loadModel(defaultModel);
-  const includedVariables = variables.filter(
-    (v) => v.category === "Floating Species",
-  );
+const models = {
+  "default model": defaultModel,
+  "kholodenko medium": kholodenkoMedium,
+  "firczuk large": firczukLarge,
+  "smallbone xlarge": smallboneXlarge,
+};
 
-  bench(
-    "default ant",
-    async () => {
-      await simulator.simulateTimeCourse(defaultModel, {
-        parameters: {
-          startTime: 0,
-          endTime: 20,
-          includedVariables: includedVariables,
-          numberOfPoints: 200,
-          resetInitialConditions: false,
-        },
-        variableValues: {},
-      });
-    },
-    { warmupIterations: 5, iterations: 100 },
-  );
-}
+describe("simple time course", async () => {
+  for (const [name, code] of Object.entries(models)) {
+    const simulator = new CopasiSimulator();
+    const variables = await simulator.loadModel(code);
+    const includedVariables = variables.filter(
+      (v) => v.category === "Floating Species",
+    );
 
-{
-  const simulator = new CopasiSimulator();
-  const variables = await simulator.loadModel(kholodenkoMedium);
-  const includedVariables = variables.filter(
-    (v) => v.category === "Floating Species",
-  );
-
-  bench(
-    "kholodenko medium",
-    async () => {
-      await simulator.simulateTimeCourse(kholodenkoMedium, {
-        parameters: {
-          startTime: 0,
-          endTime: 30,
-          includedVariables: includedVariables,
-          numberOfPoints: 400,
-          resetInitialConditions: false,
-        },
-        variableValues: {},
-      });
-    },
-    { warmupIterations: 5, iterations: 250 },
-  );
-}
-
-{
-  const simulator = new CopasiSimulator();
-  const variables = await simulator.loadModel(firczukLarge);
-  const includedVariables = variables.filter(
-    (v) => v.category === "Floating Species",
-  );
-
-  bench(
-    "firczuk large",
-    async () => {
-      await simulator.simulateTimeCourse(firczukLarge, {
-        parameters: {
-          startTime: 0,
-          endTime: 30,
-          includedVariables: includedVariables,
-          numberOfPoints: 300,
-          resetInitialConditions: false,
-        },
-        variableValues: {},
-      });
-    },
-    { warmupIterations: 3, iterations: 50 },
-  );
-}
-
-{
-  const simulator = new CopasiSimulator();
-  const variables = await simulator.loadModel(smallboneXlarge);
-  const includedVariables = variables.filter(
-    (v) => v.category === "Floating Species",
-  );
-
-  bench(
-    "smallbone xlarge",
-    async () => {
-      await simulator.simulateTimeCourse(smallboneXlarge, {
-        parameters: {
-          startTime: 0,
-          endTime: 30,
-          includedVariables: includedVariables,
-          numberOfPoints: 300,
-          resetInitialConditions: false,
-        },
-        variableValues: {},
-      });
-    },
-    { warmupIterations: 3, iterations: 25 },
-  );
-}
+    bench(
+      name,
+      async () => {
+        await simulator.simulateTimeCourse(code, {
+          parameters: {
+            startTime: 0,
+            endTime: 30,
+            includedVariables: includedVariables,
+            numberOfPoints: 300,
+            resetInitialConditions: true,
+          },
+          variableValues: {},
+        });
+      },
+      { warmupIterations: 5 },
+    );
+  }
+});
